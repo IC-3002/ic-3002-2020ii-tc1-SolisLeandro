492a3139e244dbd0e7122b0683dbd7cb652480ba
FloatingPoint <floatingpoint@FloatingPoint.floatingpoint.com>
2020-09-04 15:01:09 -0600

    Codigo finalizado

--- euler.py
+++ euler.py
@@ -1,8 +1,38 @@
 def e_cuadratica(n):
-    # Implemente esta función
-    return 0
-
-
+	# Implemente esta función
+	if n < 0:
+		return -1
+	elif n == 0:
+		return 1
+	elif n == 1:
+		return 2
+	else:
+		acum=2
+		for i in range(2,n+1):
+			acum+=(1/factorial(i))
+	return acum
+	
 def e_lineal(n):
-    # Implemente esta función
-    return 0
+	# Implemente esta función
+
+	if n < 0:
+		return -1
+	elif n == 0:
+		return 1
+	elif n == 1:
+		return 2
+	else:
+		acum=2
+		fact=2
+		for i in range(2,n+1):
+			acum+=(1/fact)
+			fact*=i+1
+	return acum
+	return 0
+    
+def factorial(n):
+	fact = 1
+	if n > 0:
+   		for i in range(1,n + 1):
+   			fact = fact*i
+	return fact
